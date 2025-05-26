9e3540134edb6d1a9b6240775dcb5743a8179c4c
Nikita Ugnich <nikitagavrilin005@gmail.com>
2025-05-27 00:44:10 +0300

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+### Most useful HTTP API ever (or not)
---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    return {"endpoints": ["/get_milk"]}
+
+@app.get("/get_milk", status_code=418)
+async def get_milk():
+    return {"message": "Out of milk"}
